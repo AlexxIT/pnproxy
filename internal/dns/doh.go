6b0c14095174ca7f39605e5f649a11d6ee02fcdb
Alex X <alexey.khit@gmail.com>
2024-08-25 14:31:55 +0300

    Fix freezes in DNS DOH

--- internal/dns/doh.go
+++ internal/dns/doh.go
@@ -26,12 +26,12 @@ func newDoHConn(server string) *dohConn {
 func (d *dohConn) Read(b []byte) (n int, err error) {
 	req, ok := d.pool.Get().([]byte)
 	if !ok {
-		return
+		return 0, io.EOF
 	}
 
 	res, err := d.query(req)
 	if err != nil {
-		return
+		return 0, err
 	}
 
 	return copy(b, res), nil
@@ -60,29 +60,29 @@ func (d *dohConn) SetDeadline(t time.Time) error {
 }
 
 func (d *dohConn) SetReadDeadline(t time.Time) error {
-	return nil
+	panic("not implemented")
 }
 
 func (d *dohConn) SetWriteDeadline(t time.Time) error {
-	return nil
+	panic("not implemented")
 }
 
 func (d *dohConn) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
-	return
+	panic("not implemented")
 }
 
 func (d *dohConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
-	return
+	panic("not implemented")
 }
 
 func (d *dohConn) query(b []byte) ([]byte, error) {
-	ctx := context.Background()
-
-	if !d.deadline.IsZero() {
-		var cancel context.CancelFunc
-		ctx, cancel = context.WithDeadline(ctx, d.deadline)
-		defer cancel()
+	deadline := d.deadline
+	if deadline.IsZero() {
+		deadline = time.Now().Add(5 * time.Second)
 	}
+
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
 
 	// https://datatracker.ietf.org/doc/html/rfc8484
 	req, err := http.NewRequestWithContext(ctx, "POST", d.server, bytes.NewReader(b))
@@ -93,8 +93,7 @@ func (d *dohConn) query(b []byte) ([]byte, error) {
 	req.Header.Set("Accept", "application/dns-message")
 	req.Header.Set("Content-Type", "application/dns-message")
 
-	client := http.Client{Timeout: 5 * time.Second}
-	res, err := client.Do(req)
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
