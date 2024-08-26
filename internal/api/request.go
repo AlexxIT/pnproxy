6868bb2a01141a2a65e8a00cc7439ddb8bca3f92
Alex X <alexey.khit@gmail.com>
2024-08-26 20:39:28 +0300

    Update request API for testing

--- internal/api/request.go
+++ internal/api/request.go
@@ -6,6 +6,8 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"strings"
+	"sync"
 	"time"
 
 	ihttp "github.com/AlexxIT/pnproxy/internal/http"
@@ -13,21 +15,51 @@ import (
 )
 
 func apiRequest(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
+	urls := r.URL.Query()["url"]
 
-	req, err := http.NewRequest(query.Get("method"), query.Get("url"), nil)
+	var wg sync.WaitGroup
+	wg.Add(len(urls))
+
+	results := make([]any, len(urls))
+
+	for i, url := range urls {
+		go func(i int, url string) {
+			results[i] = request(url)
+			wg.Done()
+		}(i, url)
+	}
+
+	wg.Wait()
+
+	w.Header().Add("Content-Type", "application/json")
+	e := json.NewEncoder(w)
+	e.SetIndent("", "  ")
+	_ = e.Encode(results)
+}
+
+func request(url string) any {
+	if strings.Index(url, "://") < 0 {
+		url = "https://" + url
+	}
+
+	result := &struct {
+		URL        string   `json:"url"`
+		Addrs      []string `json:"dns_address,omitempty"`
+		Proto      string   `json:"proto,omitempty"`
+		StatusCode int      `json:"status_code,omitempty"`
+		Location   string   `json:"location,omitempty"`
+		Error      string   `json:"error,omitempty"`
+	}{
+		URL: url,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		result.Error = err.Error()
+		return result
 	}
 
-	var result struct {
-		Addrs      []string          `json:"dns_address"`
-		URL        string            `json:"url"`
-		Proto      string            `json:"proto"`
-		StatusCode int               `json:"status_code"`
-		Headers    map[string]string `json:"headers"`
-	}
+	result.Addrs, _ = net.LookupHost(req.URL.Host)
 
 	var res *http.Response
 
@@ -36,13 +68,9 @@ func apiRequest(w http.ResponseWriter, r *http.Request) {
 		rec := httptest.NewRecorder()
 		ihttp.Handle(rec, req)
 		res = rec.Result()
-		res.Request = req
 	case "https":
 		transport := &http.Transport{
 			DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-				host, _, _ := net.SplitHostPort(addr)
-				result.Addrs, _ = net.LookupHost(host)
-
 				conn1, conn2 := net.Pipe()
 				go tls.Handle(conn2)
 				return conn1, nil
@@ -55,26 +83,20 @@ func apiRequest(w http.ResponseWriter, r *http.Request) {
 		}
 		res, err = transport.RoundTrip(req)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			result.Error = err.Error()
+			return result
 		}
 		_ = res.Body.Close()
-	default:
-		http.Error(w, "wrong scheme", http.StatusBadRequest)
-		return
 	}
 
-	result.URL = res.Request.URL.String()
-	result.Proto = res.Proto
-	result.StatusCode = res.StatusCode
-
-	result.Headers = make(map[string]string)
-	for k, v := range res.Header {
-		result.Headers[k] = v[0]
+	if res != nil {
+		result.URL = res.Request.URL.String()
+		result.Proto = res.Proto
+		result.StatusCode = res.StatusCode
+		if location := res.Header.Get("Location"); location != "" {
+			result.Location = location
+		}
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-	e := json.NewEncoder(w)
-	e.SetIndent("", "  ")
-	_ = e.Encode(result)
+	return result
 }
